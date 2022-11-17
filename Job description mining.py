c00420c225a2b3d7b74af03e899fae41d2669908
DaoMinhHang <95505320+DaoMinhHang@users.noreply.github.com>
2022-11-17 21:38:34 +0700

    Update Job description mining.py

--- Job description mining.py
+++ Job description mining.py
@@ -33,7 +33,7 @@ IT_data=data_preprocessed[data_preprocessed.IT ==True]
 IT_data=IT_data.dropna()
 IT_data['Year'].value_counts()
 
-IT_data_2005_2011 = IT_data[IT_data['Year'].isin([2005, 2006, 2007, 2008, 2009, 2010, 2011]) ]
+IT_data_2005_2008 = IT_data[IT_data['Year'].isin([2005, 2006, 2007, 2008]) ]
 IT_data_2012_2015 = IT_data[IT_data['Year'].isin([2012, 2013, 2014, 2015]) ]
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
@@ -41,11 +41,11 @@ lemmatizer = WordNetLemmatizer()
 
 qualifications_2012_15=IT_data_2012_2015['RequiredQual']
 qualifications_2012_15=qualifications_2012_15.tolist()
-qualifications_2005_11=IT_data_2005_2011['RequiredQual']
-qualifications_2005_11=qualifications_2005_11.tolist()
+qualifications_2005_08=IT_data_2005_2008['RequiredQual']
+qualifications_2005_08=qualifications_2005_08.tolist()
 
 
-data_words = list(sent_to_words(qualifications_2005_11))
+data_words = list(sent_to_words(qualifications_2005_08))
 from nltk.corpus import stopwords
 stop_words=stopwords.words('english')
 stop_words.extend(['candidates','implementation','qualification', 'qualifications', 'education'
@@ -56,51 +56,51 @@ stop_words.extend(['candidates','implementation','qualification', 'qualification
                    'quality', 'tool', 'personality', 'familiarity', 'strong', 'remuneration', 'note', 'notes', 'competency', 'organization', 'organisation',
                    'language', 'proficiency', 'advantage', 'solving', 'program', 'application', 'applications', 'professionalism', 'competency', 'person', 'people',
                    'passion','passionate','understanding','expertise'])
-pos_tags_qual_2005_11=[]
-for q in qualifications_2005_11:
-    q_tokenized_2005_11= word_tokenize(q)
-    pos_tag_qual_2005_11=nltk.pos_tag(q_tokenized_2005_11)
-    pos_tags_qual_2005_11.append(pos_tag_qual_2005_11)
+pos_tags_qual_2005_08=[]
+for q in qualifications_2005_08:
+    q_tokenized_2005_08= word_tokenize(q)
+    pos_tag_qual_2005_08=nltk.pos_tag(q_tokenized_2005_08)
+    pos_tags_qual_2005_08.append(pos_tag_qual_2005_08)
 
-content_list_2005_11=[]
-for pos_pairs in pos_tags_qual_2005_11:
-    qual_content_2005_11=[]
+content_list_2005_08=[]
+for pos_pairs in pos_tags_qual_2005_08:
+    qual_content_2005_08=[]
     for p in pos_pairs:
         if p[1]=='NNP' or p[1]=='NN' or p[1]== 'NNS' or p[1]=='NNPS':
-            qual_content_2005_11.append(p[0])
-    content_list_2005_11.append(qual_content_2005_11)
+            qual_content_2005_08.append(p[0])
+    content_list_2005_08.append(qual_content_2005_08)
     
-removestopword_2005_11_list=[]
-for l in content_list_2005_11:
-    removestopword_2005_11=[]
+removestopword_2005_08_list=[]
+for l in content_list_2005_08:
+    removestopword_2005_08=[]
     for w in l:
         w=w.lower()
         if w not in stop_words:
-            removestopword_2005_11.append(w)
-    removestopword_2005_11_list.append(removestopword_2005_11)
+            removestopword_2005_08.append(w)
+    removestopword_2005_08_list.append(removestopword_2005_08)
             
 
    
 # from nltk.stem.porter import *
 
-content_list_filtered_2005_11=[]
-for l in removestopword_2005_11_list:
-    content_filtered_2005_11=[]
+content_list_filtered_2005_08=[]
+for l in removestopword_2005_08_list:
+    content_filtered_2005_08=[]
     for w in l:
 
         w=w.lower()
     # w=lemmatizer.lemmatize(w)
         w=lemmatizer.lemmatize(w)
-        content_filtered_2005_11.append(w)
-    content_list_filtered_2005_11.append(content_filtered_2005_11)
+        content_filtered_2005_08.append(w)
+    content_list_filtered_2005_08.append(content_filtered_2005_08)
             
-converted_list_2005_11=[]
-for l in content_list_filtered_2005_11:
+converted_list_2005_08=[]
+for l in content_list_filtered_2005_08:
     joined_l=' '.join(l)
-    converted_list_2005_11.append(joined_l)
+    converted_list_2005_08.append(joined_l)
 
-filtered_2005_11=[]
-for sent in converted_list_2005_11:
+filtered_2005_08=[]
+for sent in converted_list_2005_08:
     nums='0123456789'
     puncs='''~`!@$%^&*()-_=:;<>,.?\|'"'''
     sent_filtered=''
@@ -112,9 +112,9 @@ for sent in converted_list_2005_11:
         if c in puncs:
             c=''
         sent_filtered+=c
-    filtered_2005_11.append(sent_filtered)
+    filtered_2005_08.append(sent_filtered)
 data_lemmatized =[]
-for i in filtered_2005_11:
+for i in filtered_2005_08:
   i=i.split()
   data_lemmatized.append(i)
 
@@ -130,18 +130,6 @@ texts = data_lemmatized
 # Term Document Frequency
 corpus = [id2word.doc2bow(text) for text in texts]
 
-# lda_model = gensim.models.LdaMulticore(corpus=corpus,
-#                                        id2word=id2word,
-#                                        num_topics=10, 
-#                                        random_state=100,
-#                                        chunksize=100,
-#                                        passes=10,
-#                                        per_word_topics=True)
-
-# from pprint import pprint
-# # Print the Keyword in the 10 topics
-# pprint(lda_model.print_topics(num_words=20))
-# doc_lda = lda_model[corpus]
 
 from gensim.models import CoherenceModel
 # # Compute Coherence Score
